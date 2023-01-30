7d446ba63ee5986b03452a1ebbce8b95160d959b
anakon73 <nazarkinah73@gmail.com>
2023-01-30 20:35:52 +0200

    feat(classcounter): create first class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 const App = () => {
-  const [value, setValue] = useState("Text");
-
   return (
     <div>
       <Counter />
+      <ClassCounter />
     </div>
   );
 };
