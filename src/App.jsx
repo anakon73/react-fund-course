582ef9100510fe86ebe48c96f7355a78ae290e8b
anakon73 <nazarkinah73@gmail.com>
2023-01-30 20:25:41 +0200

    feat(counter): create first functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
 
-function App() {
-  const [likes, setLikes] = useState(0);
+const App = () => {
   const [value, setValue] = useState("Text");
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-  function decrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <div>
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
     </div>
   );
-}
+};
 
 export default App;
