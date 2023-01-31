b533ab5d7823d6a99ecf7da321d981a3c2297e5d
anakon73 <nazarkinah73@gmail.com>
2023-01-31 15:10:10 +0200

    feat(postlist): created component and worked with lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import PostItem from "./components/PostItem";
+import { useState } from "react";
+import PostList from "./components/PostList";
 import "./styles/App.css";
 
 const App = () => {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python", body: "Description" },
+    { id: 2, title: "Python 2", body: "Description" },
+    { id: 3, title: "Python 3", body: "Description" },
+  ]);
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
+      <PostList posts={posts} title={"Посты про JS"} />
+      <PostList posts={posts2} title={"Посты про Python"} />
     </div>
   );
 };
