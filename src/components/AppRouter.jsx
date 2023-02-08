1b0d4950377e167e984d1d31959440bfbf46d4bd
anakon73 <nazarkinah73@gmail.com>
2023-02-08 20:02:14 +0200

    feat(postidpage): created page for post that got by id

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,12 +2,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Error from "../pages/Error";
 import Posts from "../pages/Posts";
 import About from "../pages/About";
+import PostIdPage from "../pages/PostIdPage";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/about" element={<About />} />
       <Route path="/posts" element={<Posts />} />
+      <Route path="/posts/:id" element={<PostIdPage />} />
       <Route path="/error" element={<Error />} />
       <Route path="*" element={<Navigate to="/error" />} />
     </Routes>
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,6 +1,9 @@
 import MyButton from "./UI/button/MyButton";
+import { useHistory, useNavigate } from "react-router-dom";
 
 const PostItem = ({ post, remove }) => {
+  const router = useNavigate();
+
   return (
     <div className="post">
       <div className="post__content">
@@ -10,6 +13,13 @@ const PostItem = ({ post, remove }) => {
         <div>{post.body}</div>
       </div>
       <div className="post__btns">
+        <MyButton
+          onClick={() => {
+            router(`/posts/${post.id}`);
+          }}
+        >
+          Открыть
+        </MyButton>
         <MyButton
           onClick={() => {
             remove(post);
